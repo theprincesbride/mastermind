0970257f7cf55abd9f6716e252696993568c500a
theprincesbride <140436808+theprincesbride@users.noreply.github.com>
2023-10-30 17:26:07 -0400

    Made a functioning game of mastermind with both computer and human codebreakers and codemakers.  The computer is not very smart as a guesser, but it takes feedback into consideration.

--- script.rb
+++ script.rb
@@ -19,12 +19,27 @@ display_gameboard(gameboard.board)
     turn = 0
     until turn == 12
       master_copy = master_sequence.clone
+      feedback_copy = codemaker.feedback.clone
       display_gameboard(gameboard.board)
-      guess_sequence = codebreaker.guess()
+      sleep 2
+      if codebreaker.type == "human"
+        guess_sequence = codebreaker.guess()
+      elsif codebreaker.type == "computer" && turn == 0
+        guess_sequence = codebreaker.guess()
+        previous_guess = guess_sequence.clone
+      elsif codebreaker.type == "computer" && turn > 0
+        guess_sequence = codebreaker.guess2(feedback_copy, previous_guess)
+        previous_guess = guess_sequence.clone
+      end
       gameboard.board[turn].shift
       gameboard.board[turn].unshift(guess_sequence)
       display_gameboard(gameboard.board)
-      feedback_array = codemaker.guess_feedback(guess_sequence, master_copy)
+      if codemaker.type == "computer"
+        feedback_array = codemaker.guess_feedback(guess_sequence, master_copy)
+      elsif codemaker.type == "human"
+        feedback_array = codemaker.guess_feedback()
+        codemaker.feedback = feedback_array.clone
+      end
       gameboard.board[turn][1][:codekey] = feedback_array
       display_gameboard(gameboard.board)
       if feedback_array == ["X", "X", "X", "X"] && codebreaker.type == "human"
@@ -45,8 +60,26 @@ display_gameboard(gameboard.board)
         else
             puts "Do you want to play again?  Type yes, no, y or n."
         end
+        end
+      elsif feedback_array == ["X", "X", "X", "X"] && codebreaker.type == "computer"
+        puts "I'm sorry, your opponent guessed the correct sequence.  You lost!"
+        puts "m---(v_v)---m"
+        sleep 2
+        puts "Don't give up!  Do you want to try again?"
+        loop do
+        game_choice = gets
+        game_choice = game_choice.chomp.downcase
+        if game_choice == "yes" || game_choice == "y"
+            puts "Yay!  You chose to play a new game!"
+            sleep 2
+            game()
+        elsif game_choice == "no" || game_choice == "n"
+            puts "Thank you for playing Mastermind.  Have a great day!"
+            exit
+        else
+            puts "Do you want to play again?  Type yes, no, y or n."
+        end
       end
-
       end
       turn += 1
     end
@@ -68,17 +101,38 @@ display_gameboard(gameboard.board)
     else
         puts "Do you want to play again?  Type yes, no, y or n."
     end
+
+  end
+elsif codemaker.type == "human"
+  puts "Congratulations!  Your opponent could not guess the correct sequence!  You won against the Codebreaker!"
+  puts "W---(^_^)---W"
+  sleep 2
+  puts "Do you want to start a new game?"
+  loop do
+  game_choice = gets
+  game_choice = game_choice.chomp.downcase
+  if game_choice == "yes" || game_choice == "y"
+      puts "Yay!  You chose to play a new game!"
+      sleep 2
+      game()
+  elsif game_choice == "no" || game_choice == "n"
+      puts "Thank you for playing Mastermind.  Have a great day!"
+      exit
+  else
+      puts "Do you want to play again?  Type yes, no, y or n."
   end
   end
+end
 end
 
 
 
 class CodemakerHuman
-  attr_accessor :choices, :type
+  attr_accessor :choices, :type, :feedback
   def initialize()
     @choices = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
     @type = "human"
+    @feedback = []
   end
 
   def make_code_sequence()
@@ -117,6 +171,26 @@ class CodemakerHuman
       end
       code_array
     end
+
+  def guess_feedback()
+    feedback_array = []
+    puts "Please provide feedback to your opponent on how well their guess aligns with the master code sequence.  Put an 'X' if they guessed the right color in the right position.  Put an 'O' if they guessed the right color in the wrong position.  Put a '-' if they guessed the wrong color in the wrong position.  These don't have to be in any particular order."
+    i = 0
+    until i == 4
+      feedback_choice = gets
+      feedback_choice = feedback_choice.chomp.upcase
+      if ["X", "O", "-"].include?(feedback_choice)
+        puts "You chose #{feedback_choice} to provide feedback to your opponent."
+        feedback_array.push(feedback_choice)
+        p feedback_array
+        i += 1
+      else
+        puts "Please choose 'X', 'O' or '-' to provide feedback to your opponent."
+      end
+    end
+    p feedback_array
+    feedback_array
+  end
 end
 
 class CodemakerComputer < CodemakerHuman
@@ -222,6 +296,65 @@ class CodebreakerComputer < CodebreakerHuman
     @type = "computer"
   end
 
+  def guess()
+    guess_array = []
+    i = 0
+    while i < 4
+      code = @choices.sample
+      guess_array.push(code)
+      i += 1
+    end
+    guess_array
+    p guess_array
+  end
+
+  def guess2(feedback_array, previous_array)
+    guess_array = ["-", "-", "-", "-"]
+    previous_array_copy = previous_array.clone
+   # binding.pry
+    i = 0
+    while i < 4
+      if feedback_array[i] == "X"
+        code = previous_array_copy.sample
+        code_index = previous_array_copy.index(code)
+        previous_array_copy.delete_at(code_index)
+        guess_array.delete_at(code_index)
+        guess_array.insert(code_index, code)
+        i += 1
+      else
+        i += 1
+      end
+    end
+    j = 0
+    while j < 4
+      if feedback_array[j] == "O"
+        code = previous_array_copy.sample
+        prv_code_index = previous_array_copy.index(code)
+        previous_array_copy.delete_at(prv_code_index)
+        code_index = guess_array.index("-")
+        guess_array.delete_at(code_index)
+        guess_array.insert(code_index, code)
+        j += 1
+      else
+        j += 1
+      end
+    end
+    k = 0
+    while k < 4
+      if guess_array[k] == "-"
+      code = @choices.sample
+      guess_array.delete_at(k)
+      guess_array.insert(k, code)
+      k += 1
+      else
+        k += 1
+      end
+    end
+    guess_array
+    p guess_array
+  end
+
+
 end
 
 def display_gameboard(array)
@@ -287,6 +420,4 @@ class Gameboard
   end
 end
 
-##game()
-human = CodemakerHuman.new
-human.make_code_sequence()
+game()
